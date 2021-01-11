5b6f78310465bedd0f71a5d8c4edc100c873fbce
Julius <kohwoojin@gmail.com>
2021-01-11 01:23:06 -0500

    Siugnal fix

--- games/views.py
+++ games/views.py
@@ -11,11 +11,7 @@ from twilio.jwt.access_token.grants import VideoGrant
 from dotenv import load_dotenv
 from django.contrib import messages
 from .forms import UserCreationForm, ProfileForm, UserSearchForm, UserSelectForm
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth.signals import user_logged_out
-from .models import Profile
+
 
 
 load_dotenv()
@@ -141,19 +137,3 @@ def add_friends(request):
 
     else:
         return redirect('people')
-
-
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    try:
-        profile = instance.profile
-    except ObjectDoesNotExist:
-        profile = Profile.objects.create(user=instance)
-        profile.save()
-
-
-@receiver(user_logged_out)
-def create_or_update_user_profile(sender, request, user, **kwargs):
-    if user and user.profile and user.profile.online:
-        user.profile.online = False
-        user.profile.save()
