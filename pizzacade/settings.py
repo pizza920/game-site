74adcf6307459527f513553789075234c408ad80
Julius <kohwoojin@gmail.com>
2021-01-28 15:09:09 -0500

    Fix for settings to allow pizzacade as url

--- pizzacade/settings.py
+++ pizzacade/settings.py
@@ -7,7 +7,7 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'pizzacade.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
