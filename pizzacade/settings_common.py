dd14a3b88742311330e00624de8f34b0a2a7068f
Julius <kohwoojin@gmail.com>
2021-01-31 19:10:30 -0500

    File fix

--- pizzacade/settings_common.py
+++ pizzacade/settings_common.py
@@ -15,7 +15,7 @@ import django_on_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -115,7 +115,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 APPEND_SLASH = True
 
