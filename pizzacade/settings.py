47fa970d05014eefa556438b4c1f33553d07c060
Julius <kohwoojin@gmail.com>
2021-02-01 23:59:59 -0500

    Static file fix

--- pizzacade/settings.py
+++ pizzacade/settings.py
@@ -41,16 +41,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'games/static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-print("BASE DIR: " + BASE_DIR)
-print("BASE DIR: " + BASE_DIR)
-print("BASE DIR: " + BASE_DIR)
-print("BASE DIR: " + BASE_DIR)
 
 django_on_heroku.settings(locals())
