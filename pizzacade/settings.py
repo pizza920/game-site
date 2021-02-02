c4a4a10569db0d160e8d05e6c8bde449364c57db
Julius <kohwoojin@gmail.com>
2021-02-02 00:35:54 -0500

    Fix files

--- pizzacade/settings.py
+++ pizzacade/settings.py
@@ -56,7 +56,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
