7344d64dc48c8f20c5d8b40ee90285bcb4b9117c
Julius <kohwoojin@gmail.com>
2021-02-02 00:23:06 -0500

    Static files

--- pizzacade/settings.py
+++ pizzacade/settings.py
@@ -55,7 +55,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'games/static'),
 )
 
 django_on_heroku.settings(locals())
