509e9d91cc240f1f8d0f0bbf181e6e730d6dc076
Julius <kohwoojin@gmail.com>
2021-02-02 21:02:59 -0500

    Cloudinary config

--- games/models.py
+++ games/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from cloudinary.models import CloudinaryField
 
 
 TEMPERAMENT_CHOICES = (
@@ -41,7 +42,7 @@ class Profile(models.Model):
     education_preference = models.CharField(null=True, blank=True, choices=EDUCATION_CHOICES, max_length=24)
     friends = models.ManyToManyField(to=User, related_name='profile_friends', blank=True)
     online_count = models.IntegerField(default=0)
-    picture = models.ImageField(upload_to='images/', null=True, blank=True)
+    picture = CloudinaryField('avatar')
 
     def as_dict(self):
         picture = None
