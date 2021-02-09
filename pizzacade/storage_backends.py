05a44e21544e38138bb337cdc05e8edbad5e3a60
Julius <kohwoojin@gmail.com>
2021-02-09 00:51:23 -0500

    File storage changes

--- pizzacade/storage_backends.py
+++ pizzacade/storage_backends.py
@@ -4,7 +4,3 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
-
-
-class StaticStorage(S3Boto3Storage):
-    default_acl = 'public-read'
